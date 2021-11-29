7c968e65f0ed4a1d19544a9427d3966360d3e549
yuki20020929@gmail.com <yuki20020929@gmail.com>
2021-11-29 23:20:30 +0900

    yorosiku

---
+++ Triangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructorSampe
+{
+    /// <summary>
+    /// 三角形
+    /// </summary>
+    class Triangle
+    {
+        private float width;//　幅
+        private float height;//　高さ
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="w">幅<
+        /// <param name="h">高さ</param>
+        public Triangle(float w, float h)
+        {
+            this.width = w;
+            this.height = h;
+        }
+        /// <summary>
+        /// 面積を取得
+        /// </summary>
+        /// <returns>面積</returns>
+        public float GetSurface()
+        {
+            return (width * height / 2);
+        }
+    }
+}
+
